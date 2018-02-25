70074ef0848f8b0a40240dbf206b24f3f31c9c60
Xianrun QU <xqu25@wisc.edu>
2018-02-25 12:37:01 -0600

    Update LinkedLoopIterator.java

--- P2/LinkedLoopIterator.java
+++ P2/LinkedLoopIterator.java
@@ -1,28 +1,30 @@
 import java.util.*;
-import java.io.*;
-public class LinkedLoopIterator<E> implements Iterable<E>{
+public class LinkedLoopIterator<E> implements Iterator<E>{
     private DblListnode<E> curr;
     private DblListnode<E> head;
-    public LinkedLoopIterator<E>(DblListnode<E> pass){
+    
+    		@Override
+    		public boolean hasNext(){
+    	        if(curr==null || curr==head)
+    	            return false;
+    	        return true;
+    	    }
+    		@Override
+    	    public E next(){
+    	        if(!this.hasNext())
+    	            throw new NoSuchElementException();
+    	        E ret=curr.getData();
+    	        curr=curr.getNext();
+    	        return ret;
+    	    }
+    		@Override
+    	    public void remove(){
+    	        throw new UnsupportedOperationException();
+    	    }
+    	
+    public LinkedLoopIterator(DblListnode<E> pass){
         this.curr=pass;
         this.head=pass;
     }
-    @Override
-    public boolean hasNext(){
-        if(curr==null || curr==head)
-            return false;
-        return true;
-    }
-    @Override
-    public E next(){
-        if(!this.hasNext())
-            throw new NoSuchElementException();
-        E ret=curr.getData();
-        curr=curr.getNext();
-        return ret;
-    }
-    @Override
-    public void remove(){
-        throw new UnsupportedEncodingException();
-    }
+    
 }
