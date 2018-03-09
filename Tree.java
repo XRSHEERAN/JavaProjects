01f3649107c6132ea8e69a6235fd5f1c8dcd038e
Xianrun QU <xqu25@wisc.edu>
2018-03-08 20:36:07 -0600

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -1,2 +1,10 @@
 public boolean isBinary( Treenode<T> n ){
+  if(n==null || n.getChildren().size()==0){
+    return ture;
+  }
+  List<TreeNode<T>> tbl=n.getChildren();
+  if(tbl.length==1 || tbl.size()>2){
+    return flase;
+  }
+  return (isBinary(tbl.get(0)) && isBinary(tbl.get(1)));
 }
