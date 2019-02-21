abcaae438f5e1cb76a31fb91d86baff0ea491459
Xianrun QU <xqu25@wisc.edu>
2019-02-21 17:12:09 -0600

    Update markdownCal.java

--- My_Calculator/markdownCal.java
+++ My_Calculator/markdownCal.java
@@ -3,7 +3,10 @@ public int matrix_dot(int[][] a, int[][] b){
   if(a.length==0 || a[0].length==0 || b.length==0 || b[0].length==0 || a[0].length!=b.length){
     return -1;
   }
-  
-  
+  int[][] result=[a.length][b[0].length];
+  for(int i=0;i<a.length;++i){
+    for(int j=0;j<b[0].length;++j){
+    }
+  }
   
 }
