6fdd61e9962d8c58dbd276a8645b40f0bc00b839
Xianrun QU <xqu25@wisc.edu>
2019-02-21 17:05:41 -0600

    Update markdownCal.java

--- My_Calculator/markdownCal.java
+++ My_Calculator/markdownCal.java
@@ -1,1 +1,9 @@
 //Input  a string in markdown formula and calculate the result
+public int matrix_dot(int[][] a, int[][] b){
+  if(a.length==0 || a[0].length==0 || b.length==0 || b[0].length==0 || a[0].length!=b.length){
+    return -1;
+  }
+  
+  
+  
+}
